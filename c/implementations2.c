9eb9949ae0166bd27f7b0e07d334733f93e6b6fb
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-04-12 16:09:04 -0500

    implementation additions

---
+++ c/implementations2.c
@@ -0,0 +1,63 @@
+char __VERIFIER_nondet_char() {
+    return 'a';
+}
+
+int __VERIFIER_nondet_int() {
+    return 0;
+}
+
+_Bool __VERIFIER_nondet_bool() {
+    return 1;
+}
+_Bool __VERIFIER_nondet__Bool(void) {
+    return 1;
+}
+
+char __VERIFIER_nondet_int8_t(void) {
+    return 0;
+}
+
+char __VERIFIER_nondet_msg_t(void) {
+    return 'a';
+}
+static int a[10000];
+
+void* __VERIFIER_nondet_pointer() {
+    return a;
+}
+
+void __VERIFIER_error() {
+    ;
+}
+void __VERIFIER_assume(int expression) { if (!expression) { LOOP: goto LOOP; }; return; }
+
+long  __VERIFIER_nondet_long() {
+    return 1L;
+}
+
+unsigned long __VERIFIER_nondet_ulong() {
+    return 1;
+}
+
+unsigned int __VERIFIER_nondet_uint () {
+    return 1;
+}
+
+void assert_fail(void) {
+    ;
+}
+
+double __VERIFIER_nondet_double(){
+    return 1.;
+}
+
+float __VERIFIER_nondet_float(){
+    return 1.0f;
+}
+
+
+unsigned char __VERIFIER_nondet_unsigned_char(void) {
+    return 1;
+}
+
+
